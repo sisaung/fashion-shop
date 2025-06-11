08ca8bdfe4231ab6200b413a11a9a9ae2b5a132e
sisaung <sisaungvipse780@gmail.com>
2025-06-11 20:27:33 +0630

    feat: edit product type for fit and size

--- resources/js/sizeTag.js
+++ resources/js/sizeTag.js
@@ -1,45 +1,47 @@
-
 const initializeTagActive = () => {
-    const sizeTag = document.querySelector('.size-tags');
-    const sizeHidden = document.querySelector('.size-hidden');
+    const sizeTag = document.querySelector(".size-tags");
+    const sizeHidden = document.querySelector(".size-hidden");
 
-    let selectedIds = sizeHidden.value ? sizeHidden.value.split(',').map(id => parseInt(id)) : [];
-    
-    
-   
-    const updateHiddenInput = () => {
-        sizeHidden.value = selectedIds.join(',');
+    let selectedIds = sizeHidden.value
+        ? sizeHidden.value.split(",").map((id) => parseInt(id))
+        : [];
+
+    if (selectedIds) {
+        sizeTag.querySelectorAll(".size-tag").forEach((tag) => {
+            tag.classList.remove("selected");
+            if (selectedIds.includes(parseInt(tag.dataset.id))) {
+                tag.classList.add("selected");
+            }
+        });
     }
+
+    const updateHiddenInput = () => {
+        sizeHidden.value = selectedIds.join(",");
+    };
 
     const handleFitTag = (e) => {
-        const tag = e.target.closest('.size-tag')
+        const tag = e.target.closest(".size-tag");
 
-        if(!tag) return;
-       const id = tag.dataset.id
+        if (!tag) return;
+        const id = parseInt(tag.dataset.id);
 
-       if(selectedIds) {
-        tag.classList.add('selected');
-    }
-        
-
-        if(selectedIds.includes(id)) {
-        
-            selectedIds = selectedIds.filter(el => el !== id )
-            tag.classList.remove('selected')
-
-
+        if (selectedIds) {
+            tag.classList.add("selected");
         }
-        else {
+
+        if (selectedIds.includes(id)) {
+            selectedIds = selectedIds.filter((el) => el !== id);
+            tag.classList.remove("selected");
+        } else {
             selectedIds.push(id);
-            tag.classList.add('selected');
-
+            tag.classList.add("selected");
         }
-        
+
         console.log(selectedIds);
         updateHiddenInput();
-    }
+    };
 
-    sizeTag.addEventListener('click',handleFitTag) 
-}
+    sizeTag.addEventListener("click", handleFitTag);
+};
 
-document.addEventListener('DOMContentLoaded',initializeTagActive)
+document.addEventListener("DOMContentLoaded", initializeTagActive);
