5528662e8b29bcdf886a26e9479941142daa316d
sisaung <sisaungvipse780@gmail.com>
2025-07-02 11:36:44 +0630

    change deliver info list style

--- resources/js/order-confirmation/deliveryInformation.js
+++ resources/js/order-confirmation/deliveryInformation.js
@@ -1,16 +1,30 @@
 const initalizeDeliveryInformation = () => {
-    const selectAddress = document.querySelectorAll(".select-address");
-
+    const selectAddress = document.querySelectorAll('.select-address');
 
     selectAddress.forEach((address) => {
+        const selectCheckIcon = address.querySelector('.select-check-icon');
 
         address.addEventListener("click", () => {
 
-            selectAddress.forEach(address => address.classList.remove("active-address"));
+            // Remove 'active-address' and hide check icon from all addresses
+            selectAddress.forEach(addr => {
+                addr.classList.remove("active-address");
+                const icon = addr.querySelector('.select-check-icon');
+                if (icon) {
+                    icon.classList.add('hidden');
+                }
+            });
+
+            // Add 'active-address' to clicked address
             address.classList.add("active-address");
 
+            // Show check icon of clicked address
+            if (selectCheckIcon) {
+                selectCheckIcon.classList.remove('hidden');
+            }
         });
     });
+
 };
 
 document.addEventListener("DOMContentLoaded", initalizeDeliveryInformation);
--- resources/views/public/order-confirmation/index.blade.php
+++ resources/views/public/order-confirmation/index.blade.php
@@ -131,14 +131,33 @@
                                     @if (Auth::check() && Auth::user()->address->count() > 0)
                                         @foreach (Auth::user()->address as $address)
                                             <div data-address-id="{{ $address->id }}"
-                                                class="border border-pearl-bush-300 rounded-lg px-6 py-4 select-address">
+                                                class="border border-pearl-bush-300 rounded-lg px-6 py-4 select-address active:scale-80 duration-300 ">
                                                 <div class="mb-3">
-                                                    <h3 class="font-heading font-semibold"> Customer Contact </h3>
+                                                    <div class="flex justify-between items-center">
+                                                        <h3 class="font-heading font-semibold"> Customer Contact </h3>
+                                                        <div class="relative">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                                class="size-10 stroke-pearl-bush-500 stroke-1">
+                                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                                    d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z" />
+                                                            </svg>
+
+                                                            {{-- check --}}
+                                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                                class="size-5 absolute top-0 left-0 translate-x-1/2 translate-y-1/2 stroke-pearl-bush-500 stroke-3 hidden select-check-icon">
+                                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                                    d="m4.5 12.75 6 6 9-13.5" />
+                                                            </svg>
+
+                                                        </div>
+                                                    </div>
                                                     <div class="flex items-center gap-x-5 ">
                                                         <div class="flex items-center gap-x-1">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                                class="size-5 text-pearl-bush-500">
+                                                                viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor" class="size-5 text-pearl-bush-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                                             </svg>
@@ -148,8 +167,8 @@
                                                         </div>
                                                         <div class="flex items-center gap-x-1">
                                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                                class="size-5 text-pearl-bush-500">
+                                                                viewBox="0 0 24 24" stroke-width="1.5"
+                                                                stroke="currentColor" class="size-5 text-pearl-bush-500">
                                                                 <path stroke-linecap="round" stroke-linejoin="round"
                                                                     d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                                                             </svg>
